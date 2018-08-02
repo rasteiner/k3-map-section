d1d866b767b4e757cb27270ffb5df71a8a9a85f2
Roman Steiner <roman@toastlab.ch>
2018-08-02 14:29:03 +0200

    oops

--- src/index.js
+++ src/index.js
@@ -10,27 +10,3 @@ panel.plugin('rasteiner/kn-map-section', {
     map: MapSection
   }
 })
-
-//replace fields section
-
-Vue.component('k-fields-section', {
-  extends: original,
-  created() {
-    this.$mapsection.registerFields(this)
-  },
-  destroyed() {
-    this.$mapsection.unregisterFields(this)
-  },
-  methods: {
-    input(values) {
-      this.values = values
-      original.options.methods.input.call(this, values)
-      this.$mapsection.updateMaps(values)
-    }
-  },
-  watch: {
-    values(newValues) {
-      this.$mapsection.updateMaps(newValues)
-    }
-  }
-})
